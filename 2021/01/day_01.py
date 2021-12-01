0aa80ebb51edfb871675967d754297349ad2ed31
Mike Bernard <mdbernard@protonmail.com>
2021-11-30 22:15:31 -0800

    add better part 2 algorithm

--- 2021/01/day_01.py
+++ 2021/01/day_01.py
@@ -5,5 +5,4 @@ data = np.loadtxt('input_1.txt')
 print(np.sum(data[1:] > data[:-1]))
 
 # part 2
-windows = np.array([np.sum(data[i-2:i+1]) for i in range(2, len(data))])
-print(np.sum(windows[1:] > windows[:-1]))
+print(np.sum(data[3:] > data[:-3]))

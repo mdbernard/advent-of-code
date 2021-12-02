09e60f3ecfd9cb01f58c55f71fd4bc00579696db
Mike Bernard <mdbernard@protonmail.com>
2021-12-01 22:47:16 -0800

    reduce linecount

--- 2021/02/day_02.py
+++ 2021/02/day_02.py
@@ -1,21 +1,16 @@
-# part 1
-data = {'forward': 0, 'up': 0, 'down': 0}
 with open('input_1.txt', 'r') as infile:
-    for line in infile:
-        line = line.strip().split(' ')
-        data[line[0]] += int(line[1])
+    lines = [line.strip().split(' ') for line in infile]
+lines = [(line[0], int(line[1])) for line in lines]
 
-x = data['forward']
-y = data['down'] - data['up']
+# part 1
+x = sum([(line[0] == 'forward')*line[1] for line in lines])
+y = sum([(line[0] == 'down')*line[1] - (line[0] == 'up')*line[1] for line in lines])
 print(x*y)
 
 # part 2
 x = d = aim = 0
-with open('input_1.txt', 'r') as infile:
-    for line in infile:
-        line = line.strip().split(' ')
-        line[1] = int(line[1])
-        aim += (line[0] == 'down')*line[1] - (line[0] == 'up')*line[1]
-        x += (line[0] == 'forward')*line[1]
-        d += aim*(line[0] == 'forward')*line[1]
+for line in lines:
+    aim += (line[0] == 'down')*line[1] - (line[0] == 'up')*line[1]
+    x += (line[0] == 'forward')*line[1]
+    d += aim*(line[0] == 'forward')*line[1]
 print(x*d)

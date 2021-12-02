3d2f6c87b52f819becfd8c9807fbfe85718ef175
Mike Bernard <mdbernard@protonmail.com>
2021-12-01 22:47:55 -0800

    change y to d

--- 2021/02/day_02.py
+++ 2021/02/day_02.py
@@ -4,8 +4,8 @@ lines = [(line[0], int(line[1])) for line in lines]
 
 # part 1
 x = sum([(line[0] == 'forward')*line[1] for line in lines])
-y = sum([(line[0] == 'down')*line[1] - (line[0] == 'up')*line[1] for line in lines])
-print(x*y)
+d = sum([(line[0] == 'down')*line[1] - (line[0] == 'up')*line[1] for line in lines])
+print(x*d)
 
 # part 2
 x = d = aim = 0

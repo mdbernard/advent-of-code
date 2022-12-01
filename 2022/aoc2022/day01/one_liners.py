2828938087b3f2a527ed2600284ea7ac22f9e8c7
Mike Bernard <mdbernard@protonmail.com>
2022-11-30 22:00:55 -0800

    remove 'for'

--- 2022/aoc2022/day01/one_liners.py
+++ 2022/aoc2022/day01/one_liners.py
@@ -1,2 +1,2 @@
-print("Part 1:", max([sum([int(weight) for weight in elf.split("\n")]) for elf in open("input.txt").read().strip().split("\n\n")]))
-print("Part 2:", sum(list(sorted([sum([int(weight) for weight in elf.split("\n")]) for elf in open('input.txt').read().strip().split("\n\n")], reverse=True))[:3]))
+print("Part 1:", max(map(lambda elf: sum(map(int, elf.split("\n"))), open("input.txt").read().strip().split("\n\n"))))
+print("Part 2:", sum(list(sorted(map(lambda elf: sum(map(int, elf.split("\n"))), open("input.txt").read().strip().split("\n\n")), reverse=True))[:3]))

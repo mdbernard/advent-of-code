866ccafcf1039059da93832effabb1aa751d24e4
Mike Bernard <mdbernard@protonmail.com>
2022-11-30 22:10:34 -0800

    remove extraneous  call

--- 2022/aoc2022/day01/one_liners.py
+++ 2022/aoc2022/day01/one_liners.py
@@ -1,2 +1,2 @@
 print("Part 1:", max(map(lambda elf: sum(map(int, elf.split("\n"))), open("input.txt").read().strip().split("\n\n"))))
-print("Part 2:", sum(list(sorted(map(lambda elf: sum(map(int, elf.split("\n"))), open("input.txt").read().strip().split("\n\n")), reverse=True))[:3]))
+print("Part 2:", sum(sorted(map(lambda elf: sum(map(int, elf.split("\n"))), open("input.txt").read().strip().split("\n\n")), reverse=True)[:3]))

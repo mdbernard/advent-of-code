e963fe81ec75f7cb9d228023e0d5dd114c556980
Mike Bernard <mdbernard@protonmail.com>
2022-11-30 22:26:34 -0800

    apply the walrus

--- 2022/aoc2022/day01/one_liners.py
+++ 2022/aoc2022/day01/one_liners.py
@@ -1,2 +1,2 @@
-print("Part 1:", max(map(lambda elf: sum(map(int, elf.split("\n"))), open("input.txt").read().strip().split("\n\n"))))
-print("Part 2:", sum(sorted(map(lambda elf: sum(map(int, elf.split("\n"))), open("input.txt").read().strip().split("\n\n")), reverse=True)[:3]))
+print("Part 1:", max(weights := list(map(lambda elf: sum(map(int, elf.split("\n"))), open("input.txt").read().strip().split("\n\n")))))
+print("Part 2:", sum(sorted(weights, reverse=True)[:3]))

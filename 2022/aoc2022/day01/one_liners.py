2d3c556768116818010c264ca0f39355fda35e80
Mike Bernard <mdbernard@protonmail.com>
2022-11-30 23:32:51 -0800

    remove reversed

--- 2022/aoc2022/day01/one_liners.py
+++ 2022/aoc2022/day01/one_liners.py
@@ -1,1 +1,1 @@
-print("Part 1:", max(weights := list(map(lambda elf: sum(map(int, elf.split("\n"))), open("input.txt").read().strip().split("\n\n")))), "\nPart 2:", sum(sorted(weights, reverse=True)[:3]))
+print("Part 1:", max(weights := list(map(lambda elf: sum(map(int, elf.split("\n"))), open("input.txt").read().strip().split("\n\n")))), "\nPart 2:", sum(sorted(weights)[-3:]))

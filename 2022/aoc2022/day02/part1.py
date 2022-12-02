0e7b693e0ede54c0ef3819bbe1cbfcd7818886c8
Mike Bernard <mdbernard@protonmail.com>
2022-12-01 21:31:48 -0800

    add day 2 solutions and one liners

---
+++ 2022/aoc2022/day02/part1.py
@@ -0,0 +1,57 @@
+from __future__ import annotations
+
+import argparse
+import os.path
+
+import pytest
+
+import support
+
+INPUT_TXT = os.path.join(os.path.dirname(__file__), 'input.txt')
+
+
+def compute(s: str) -> int:
+    score_map = {
+        "X": {"A": 4, "B": 1, "C": 7},
+        "Y": {"A": 8, "B": 5, "C": 2},
+        "Z": {"A": 3, "B": 9, "C": 6},
+    }
+
+    score = 0
+    lines = s.splitlines()
+    for line in lines:
+        p1, p2 = line.strip().split()
+        score += score_map[p2][p1]
+    return score
+
+INPUT_S = '''\
+A Y
+B X
+C Z
+'''
+EXPECTED = 15
+
+
+@pytest.mark.parametrize(
+    ('input_s', 'expected'),
+    (
+        (INPUT_S, EXPECTED),
+    ),
+)
+def test(input_s: str, expected: int) -> None:
+    assert compute(input_s) == expected
+
+
+def main() -> int:
+    parser = argparse.ArgumentParser()
+    parser.add_argument('data_file', nargs='?', default=INPUT_TXT)
+    args = parser.parse_args()
+
+    with open(args.data_file) as f, support.timing():
+        print(compute(f.read()))
+
+    return 0
+
+
+if __name__ == '__main__':
+    raise SystemExit(main())

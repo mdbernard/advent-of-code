485f987e52c892b03952822fb801ef5875361a54
Mike Bernard <mdbernard@protonmail.com>
2022-12-03 21:44:10 -0800

    add day 04 solutions and one liners

---
+++ 2022/aoc2022/day04/part2.py
@@ -0,0 +1,62 @@
+from __future__ import annotations
+
+import argparse
+import os.path
+
+import pytest
+
+import support
+
+INPUT_TXT = os.path.join(os.path.dirname(__file__), 'input.txt')
+
+
+def compute(s: str) -> int:
+    output = 0
+
+    lines = s.strip().split()
+    for line in lines:
+        line = line.strip().split(",")
+        first = line[0].split("-")
+        second = line[1].split("-")
+        first = set(range(int(first[0]), int(first[1]) + 1))
+        second = set(range(int(second[0]), int(second[1]) + 1))
+        if len(first & second) > 0:
+            output += 1
+
+    return output
+
+
+INPUT_S = '''\
+2-4,6-8
+2-3,4-5
+5-7,7-9
+2-8,3-7
+6-6,4-6
+2-6,4-8
+'''
+EXPECTED = 4
+
+
+@pytest.mark.parametrize(
+    ('input_s', 'expected'),
+    (
+        (INPUT_S, EXPECTED),
+    ),
+)
+def test(input_s: str, expected: int) -> None:
+    assert compute(input_s) == expected
+
+
+def main() -> int:
+    parser = argparse.ArgumentParser()
+    parser.add_argument('data_file', nargs='?', default=INPUT_TXT)
+    args = parser.parse_args()
+
+    with open(args.data_file) as f, support.timing():
+        print(compute(f.read()))
+
+    return 0
+
+
+if __name__ == '__main__':
+    raise SystemExit(main())
